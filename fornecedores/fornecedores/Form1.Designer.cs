dbabacaeb3a271af12ac9717d8f506832a69a61b
eduardocechinel07 <66022201+eduardocechinel07@users.noreply.github.com>
2020-09-27 01:07:48 -0300

    Add files via upload

---
+++ fornecedores/fornecedores/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Biblioteca
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSalvar = new System.Windows.Forms.Button();
+            this.textBoxTPP = new System.Windows.Forms.TextBox();
+            this.textBoxDESC = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewProduto = new System.Windows.Forms.DataGridView();
+            this.textBoxPP = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.buttonNovo = new System.Windows.Forms.Button();
+            this.buttonDeletar = new System.Windows.Forms.Button();
+            this.buttonAtualizar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ColumnId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnTPP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnPP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ColumnDESC = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProduto)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonSalvar
+            // 
+            this.buttonSalvar.Location = new System.Drawing.Point(6, 98);
+            this.buttonSalvar.Name = "buttonSalvar";
+            this.buttonSalvar.Size = new System.Drawing.Size(53, 21);
+            this.buttonSalvar.TabIndex = 0;
+            this.buttonSalvar.Text = "Salvar";
+            this.buttonSalvar.UseVisualStyleBackColor = true;
+            this.buttonSalvar.Click += new System.EventHandler(this.buttonSalvar_Click);
+            // 
+            // textBoxTPP
+            // 
+            this.textBoxTPP.Location = new System.Drawing.Point(161, 28);
+            this.textBoxTPP.Name = "textBoxTPP";
+            this.textBoxTPP.Size = new System.Drawing.Size(96, 20);
+            this.textBoxTPP.TabIndex = 1;
+            // 
+            // textBoxDESC
+            // 
+            this.textBoxDESC.Location = new System.Drawing.Point(263, 28);
+            this.textBoxDESC.Name = "textBoxDESC";
+            this.textBoxDESC.Size = new System.Drawing.Size(87, 20);
+            this.textBoxDESC.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(158, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Tipo de produto:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Descrição:";
+            // 
+            // dataGridViewProduto
+            // 
+            this.dataGridViewProduto.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewProduto.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ColumnId,
+            this.ColumnTPP,
+            this.ColumnPP,
+            this.ColumnDESC});
+            this.dataGridViewProduto.Location = new System.Drawing.Point(21, 55);
+            this.dataGridViewProduto.Name = "dataGridViewProduto";
+            this.dataGridViewProduto.Size = new System.Drawing.Size(543, 262);
+            this.dataGridViewProduto.TabIndex = 7;
+            this.dataGridViewProduto.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewProduto_CellContentClick);
+            // 
+            // textBoxPP
+            // 
+            this.textBoxPP.Location = new System.Drawing.Point(359, 29);
+            this.textBoxPP.Name = "textBoxPP";
+            this.textBoxPP.Size = new System.Drawing.Size(90, 20);
+            this.textBoxPP.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(356, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Praso:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "ID:";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Enabled = false;
+            this.textBoxId.Location = new System.Drawing.Point(21, 28);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(31, 20);
+            this.textBoxId.TabIndex = 10;
+            // 
+            // buttonNovo
+            // 
+            this.buttonNovo.Location = new System.Drawing.Point(6, 73);
+            this.buttonNovo.Name = "buttonNovo";
+            this.buttonNovo.Size = new System.Drawing.Size(53, 21);
+            this.buttonNovo.TabIndex = 12;
+            this.buttonNovo.Text = "Novo";
+            this.buttonNovo.UseVisualStyleBackColor = true;
+            this.buttonNovo.Click += new System.EventHandler(this.buttonNovo_Click);
+            // 
+            // buttonDeletar
+            // 
+            this.buttonDeletar.Location = new System.Drawing.Point(6, 46);
+            this.buttonDeletar.Name = "buttonDeletar";
+            this.buttonDeletar.Size = new System.Drawing.Size(53, 21);
+            this.buttonDeletar.TabIndex = 13;
+            this.buttonDeletar.Text = "Deletar";
+            this.buttonDeletar.UseVisualStyleBackColor = true;
+            this.buttonDeletar.Click += new System.EventHandler(this.buttonDeletar_Click);
+            // 
+            // buttonAtualizar
+            // 
+            this.buttonAtualizar.Location = new System.Drawing.Point(6, 19);
+            this.buttonAtualizar.Name = "buttonAtualizar";
+            this.buttonAtualizar.Size = new System.Drawing.Size(53, 21);
+            this.buttonAtualizar.TabIndex = 14;
+            this.buttonAtualizar.Text = "Atualizar";
+            this.buttonAtualizar.UseVisualStyleBackColor = true;
+            this.buttonAtualizar.Click += new System.EventHandler(this.buttonAtualizar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonAtualizar);
+            this.groupBox1.Controls.Add(this.buttonSalvar);
+            this.groupBox1.Controls.Add(this.buttonDeletar);
+            this.groupBox1.Controls.Add(this.buttonNovo);
+            this.groupBox1.Location = new System.Drawing.Point(583, 193);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(66, 124);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Comando";
+            // 
+            // ColumnId
+            // 
+            this.ColumnId.HeaderText = "ID";
+            this.ColumnId.Name = "ColumnId";
+            // 
+            // ColumnTPP
+            // 
+            this.ColumnTPP.HeaderText = "Tipo de produto";
+            this.ColumnTPP.Name = "ColumnTPP";
+            // 
+            // ColumnPP
+            // 
+            this.ColumnPP.HeaderText = "Descrição";
+            this.ColumnPP.Name = "ColumnPP";
+            // 
+            // ColumnDESC
+            // 
+            this.ColumnDESC.HeaderText = "Praso";
+            this.ColumnDESC.Name = "ColumnDESC";
+            this.ColumnDESC.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 329);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPP);
+            this.Controls.Add(this.dataGridViewProduto);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDESC);
+            this.Controls.Add(this.textBoxTPP);
+            this.Name = "Form1";
+            this.Text = "Biblioteca de Produtos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProduto)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSalvar;
+        private System.Windows.Forms.TextBox textBoxTPP;
+        private System.Windows.Forms.TextBox textBoxDESC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridViewProduto;
+        private System.Windows.Forms.TextBox textBoxPP;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Button buttonNovo;
+        private System.Windows.Forms.Button buttonDeletar;
+        private System.Windows.Forms.Button buttonAtualizar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnTPP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnPP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ColumnDESC;
+    }
+}
+
---
+++ fornecedores/fornecedores/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Biblioteca
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSalvar_Click(object sender, EventArgs e)
+        {
+            //Crio a estrutura da conexão com o banco e passa os parametros
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "fornecedores";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            //Realizo a conexão com o banco
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            { 
+                realizaConexacoBD.Open(); //Abre a conexão com o banco
+                //MessageBox.Show("Conexão Aberta!");
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand(); //Crio um comando SQL
+                comandoMySql.CommandText = "INSERT INTO produto (nomeProduto,descricaoProduto,prasoProduto) " +
+                    "VALUES('" + textBoxTPP.Text + "', '" + textBoxDESC.Text + "', '" + textBoxPP.Text + "')";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close(); // Fecho a conexão com o banco
+                MessageBox.Show("Inserido com sucesso"); //Exibo mensagem de aviso
+                atualizarGrid();
+                textBoxTPP.Text = "";
+                textBoxDESC.Text = "";
+                textBoxPP.Text = "";
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Não foi possivel abrir a conexão! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            atualizarGrid();
+        }
+
+        private void atualizarGrid()
+        {
+            //Crio a estrutura da conexão com o banco e passa os parametros
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "fornecedores";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            //Realizo a conexão com o banco
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open();
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand();
+                comandoMySql.CommandText = "SELECT * from produto WHERE ativoProduto = 0"; //Traz todo mundo da tabela autor
+                MySqlDataReader reader = comandoMySql.ExecuteReader();
+
+                dataGridViewProduto.Rows.Clear();//FAZ LIMPAR A TABELA
+
+                while (reader.Read())
+                {
+                    DataGridViewRow row = (DataGridViewRow)dataGridViewProduto.Rows[0].Clone();//FAZ UM CAST E CLONA A LINHA DA TABELA
+                    row.Cells[0].Value = reader.GetInt32(0);//ID
+                    row.Cells[1].Value = reader.GetString(1);//NOME
+                    row.Cells[2].Value = reader.GetString(2);//NACIONALIDADE
+                    row.Cells[3].Value = reader.GetString(3);//BIOGRAFIA
+                    dataGridViewProduto.Rows.Add(row);//ADICIONO A LINHA NA TABELA
+                }
+
+                realizaConexacoBD.Close();
+                //MessageBox.Show("Removido com sucesso"); ;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+        private void dataGridViewProduto_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataGridViewProduto.Rows[e.RowIndex].Cells[e.ColumnIndex].Value != null)
+            {
+                dataGridViewProduto.CurrentRow.Selected = true;
+                //preenche os textbox com as células da linha selecionada
+                textBoxTPP.Text = dataGridViewProduto.Rows[e.RowIndex].Cells["ColumnTPP"].FormattedValue.ToString();
+                textBoxDESC.Text = dataGridViewProduto.Rows[e.RowIndex].Cells["ColumnPP"].FormattedValue.ToString();
+                textBoxPP.Text = dataGridViewProduto.Rows[e.RowIndex].Cells["ColumnDESC"].FormattedValue.ToString();
+                textBoxId.Text = dataGridViewProduto.Rows[e.RowIndex].Cells["ColumnId"].FormattedValue.ToString();
+            }
+        }
+
+        private void buttonNovo_Click(object sender, EventArgs e)
+        {
+            limparCampos();
+        }
+
+        private void limparCampos()
+        {
+            textBoxId.Clear();
+            textBoxTPP.Clear();
+            textBoxDESC.Clear();
+            textBoxPP.Clear();
+
+        }
+
+        private void buttonDeletar_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "fornecedores";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open(); //Abre a conexão com o banco
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand(); //Crio um comando SQL
+                comandoMySql.CommandText = "UPDATE produto SET ativoProduto = 1 WHERE idProduto = " + textBoxId.Text + "";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close(); // Fecho a conexão com o banco
+                MessageBox.Show("Deletado com sucesso"); //Exibo mensagem de aviso
+                atualizarGrid();
+                limparCampos();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Não foi possivel abrir a conexão! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void buttonAtualizar_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder conexaoBD = new MySqlConnectionStringBuilder();
+            conexaoBD.Server = "localhost";
+            conexaoBD.Database = "fornecedores";
+            conexaoBD.UserID = "root";
+            conexaoBD.Password = "";
+            MySqlConnection realizaConexacoBD = new MySqlConnection(conexaoBD.ToString());
+            try
+            {
+                realizaConexacoBD.Open(); //Abre a conexão com o banco
+
+                MySqlCommand comandoMySql = realizaConexacoBD.CreateCommand(); //Crio um comando SQL
+                comandoMySql.CommandText = "UPDATE produto SET nomeProduto = '" + textBoxTPP.Text + "', " +
+                    "descricaoProduto = '" + textBoxDESC.Text + "', " +
+                    "prasoProduto = '" + textBoxPP.Text + "' WHERE idProduto = " + textBoxId.Text + "";
+                comandoMySql.ExecuteNonQuery();
+
+                realizaConexacoBD.Close(); // Fecho a conexão com o banco
+                MessageBox.Show("Atualizado com sucesso"); //Exibo mensagem de aviso
+                atualizarGrid();
+                limparCampos();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show("Não foi possivel abrir a conexão! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
